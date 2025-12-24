dd1f59de7f551ee7193a54434db1ec07f2bc83b8
Said177-0ps <guliyevsaid717@gmail.com>
2025-12-24 19:50:30 +0400

    first spring

---
+++ library/src/main/java/com/library/dea/DeaApplication.java
@@ -0,0 +1,13 @@
+package com.library.dea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeaApplication.class, args);
+	}
+
+}
---
+++ library/src/test/java/com/library/dea/DeaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.library.dea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
