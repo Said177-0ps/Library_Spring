a9a8ba9eec58fd4b5e5f65ce85996b777abf7503
Said177-0ps <guliyevsaid717@gmail.com>
2026-02-28 19:07:16 +0400

    Regex

---
+++ library/src/main/java/com/library/dea/dto/RegisterForm.java
@@ -0,0 +1,48 @@
+package com.library.dea.dto;
+
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
+public class RegisterForm {
+    @NotBlank
+    @Size(min = 4, max = 50)
+    @Pattern(
+            regexp = "^[a-zA-Z0-9_]+$",
+            message = "Username может содержать только буквы, цифры и '_'"
+    )
+    private String username;
+    @NotBlank
+    @Size(min = 8, max = 50)
+    @Pattern(
+            regexp = "^(?=.*[A-Z])(?=.*[a-z])(?=.*\\d)(?=.*[!@#$%^&*]).{8,}$",
+            message = "Password слишком слабый! Нужны цифры, разные регистры и спецсимволы"
+    )
+    private String password;
+
+    @NotBlank(message = "Подтвердите Пароль")
+    private String confirmPassword;
+
+
+    public String getConfirmPassword() {
+        return confirmPassword;
+    }
+
+    public void setConfirmPassword(String confirmPassword) {
+        this.confirmPassword = confirmPassword;
+    }
+
+    public String getUsername() {return username;}
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
